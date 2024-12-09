0340b2180f82f2373bd49ceb1248c47da04c90c4
mrtzfr <mertcanzafer06@gmail.com>
2024-12-09 16:57:59 +0300

    Refactor: Adding includes and defines to the header

--- RenderingEngine/src/DefinesAndMacros.h
+++ RenderingEngine/src/DefinesAndMacros.h
@@ -1,4 +1,22 @@
 #pragma once
+// define GLFW_INCLUDE_VULKAN macro to include vulkan header file that was included in glfw
+// define the macro before including glfw
+
+#define GLFW_INCLUDE_VULKAN
+
+#include <GLFW\glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <vulkan\vk_enum_string_helper.h>
+#include <optional>
+#include <set>
+
+const uint32_t width = 1280u, height = 720u;
 
 #ifdef NDEBUG // If not in debug mode.
 const bool enableValidationLayers{ false };
@@ -20,6 +38,20 @@ do {									\
 		throw  std::runtime_error(error);			\
 	}												\
 }while(0)
+
+static struct SwapChainSupportDetails
+{
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR> presentModes;
+};
+
+static struct QueueFamiliyIndicies
+{
+	std::optional<uint32_t> GraphicsFamily;
+	std::optional<uint32_t> PresentFamily;
+	bool IsComplete()const { return GraphicsFamily.has_value() && PresentFamily.has_value(); }
+};
 
 extern enum DevInfo
 {
